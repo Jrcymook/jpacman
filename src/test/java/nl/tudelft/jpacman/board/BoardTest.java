324ecbc2b862e977ff09672b7230854a276c5d61
Jrcymook <55121062+Jrcymook@users.noreply.github.com>
2021-11-02 15:10:55 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+/**
+ * Basic implementation of square.
+ *
+ * @author Jeroen Roosen 
+ */
 
 class BoardTest {
 
